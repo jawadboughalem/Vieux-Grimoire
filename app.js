548bc1d98d3bee475ba6610e8ac8da59843c0903
jawadboughalem <jawadboughalem91@gmail.com>
2023-09-05 11:06:36 +0200

    Routes, mongoDB, CORS

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
+//Import des routes
+const bookRoutes = require('./routes/books');
+const userRoutes = require('./routes/user');
+
+//Création app express
 const app = express();
 
-app.use(express.json());
-
-const mongoose = require('mongoose')
+//Import MongoDB
 mongoose.connect('mongodb+srv://user1:aqwzsx123@cluster0.vhogioq.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+//Accède au cœur de la requête. Intercepte les requêtes qui ont un type de contenu json et rend le contenu accessible à l'aide de req.body
+app.use(express.json());
+
+//CORS...
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -20,15 +29,6 @@ app.use((req, res, next) => {
 
 
 // Route d'accueil pour obtenir la liste des livres
-app.get('/api/books', (req, res) => {
-   // Pour le moment, je renvoie simplement un message.
-   // Plus tard, cela devrait renvoyer la liste réelle des livres depuis ma base de données.
-   res.json({ message: 'Liste des livres!' }); 
-});
-
-// Route pour l'authentification
-app.post('/api/auth/signin', (req, res) => {
-   res.json({ message: 'Authentification réussie!' }); 
-});
+app.use('/api/auth', userRoutes)
 
 module.exports = app;
