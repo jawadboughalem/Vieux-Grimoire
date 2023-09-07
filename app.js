aaed105ee06553be76fa2ebaf49c0dc3d7cb22b7
jawadboughalem <jawadboughalem91@gmail.com>
2023-09-07 16:54:04 +0200

    A jour

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use((req, res, next) => {
 
 
 // Route d'accueil pour obtenir la liste des livres
-app.use('/api/auth', userRoutes)
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
