324ad43bcb4f8e852596e229a5d1617c80a4d698
jawadboughalem <jawadboughalem91@gmail.com>
2023-09-21 15:51:47 +0200

    A jour

--- controllers/user.js
+++ controllers/user.js
@@ -1,46 +1,54 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+require("dotenv").config();
 
+//Inscription des utilisateurs
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        // Nouvel utilisateur avec le modèle
-        const user = new User({
-          email: req.body.email,
-          password: hash
-        });
-        //DB
-        user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      // Nouvel utilisateur avec le modèle
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      //DB
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
+//Connexion des utilisateurs
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    // MDP incorrect
-                    if (!valid) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
-                    }
-                    // MDP correcte
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'BOOKS_ARE_AWESOME',
-                            { expiresIn: '24h' },
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Paire login/mot de passe incorrecte" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          // MDP incorrect
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ message: "Paire login/mot de passe incorrecte" });
+          }
+          // MDP correcte
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, process.env.CLE_SECRETE, {
+              expiresIn: "24h",
+            }),
+          });
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
